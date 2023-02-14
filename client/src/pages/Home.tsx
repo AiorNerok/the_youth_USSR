15d5f3a4af8aa166ef0883139139325958c33721
AiorNerok <90855034+AiorNerok@users.noreply.github.com>
2023-02-14 13:57:12 +0300

    Add files via upload

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,11 +4,14 @@ import { HomePageProps } from "types";
 
 export const Home: FC<HomePageProps> = () => {
   return (
-    <div className="container text-3xl font-bold mx-auto font-gt">
+    <div className="container min-h-screen text-3xl font-bold mx-auto font-gt flex flex-col">
       <Header />
-      <div className="flex">
-        <div className="w-80">
+      <div className="flex flex-1">
+        <div className="w-[280px] mr-[10px]">
           <Aside />
+        </div>
+        <div className="flex-1 bg-prime-gray-3">
+
         </div>
       </div>
     </div>
