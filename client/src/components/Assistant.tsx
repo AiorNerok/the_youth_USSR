21b69ee2a590f288111fa79eb92a2f3dce12f30d
AiorNerok <90855034+AiorNerok@users.noreply.github.com>
2023-02-14 15:05:17 +0300

    Add files via upload

---
+++ client/src/components/Assistant.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+
+import Wolf from "assets/icons/wolf.svg";
+
+export const Assistant: FC = () => {
+  return (
+    <div className="w-[178px] flex flex-col relative mx-auto items-center bg-prime-green rounded-2xl py-[40px]">
+      <div className="h-[30px] absolute -top-[30px]">
+        <div className="mt-[6px] bg-prime-green rounded-full w-[60px] h-[60px] mb-[14px]">
+          <img src={Wolf} alt="assistant" className="-translate-y-[5px]" />
+        </div>
+      </div>
+      <div className="text-center text-2 text-prime-violet-2">
+        <p className="mb-[10px]">Вуфентий</p>
+        <p className="text-prime-black mx-5">
+          Если волк молчит то лучше его не перебивай.
+          <br /> АУФ!
+        </p>
+      </div>
+    </div>
+  );
+};
