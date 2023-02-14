e29ebda1a0ac8f5f779e925f4abc0be57361795c
AiorNerok <90855034+AiorNerok@users.noreply.github.com>
2023-02-14 13:35:33 +0300

    Add files via upload

--- client/src/types.tsx
+++ client/src/types.tsx
@@ -3,10 +3,11 @@ export interface HomePageProps {}
 export interface IconProps {
   pathfill?: string;
   rectfill?: string;
+  isHover?: boolean
 }
 
 export interface ListItemProps {
   text: string;
-  icon?: JSX.Element;
-  iconHover?: JSX.Element;
+  icon?: JSX.Element | string;
+  iconHover?: JSX.Element | string;
 }
