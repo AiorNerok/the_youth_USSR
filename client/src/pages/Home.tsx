3b884c4a883e7374faac37db3854828bf82f6c1f
AiorNerok <90855034+AiorNerok@users.noreply.github.com>
2023-02-16 16:18:40 +0300

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { HomePageProps } from "types";
 
 export const Home: FC<HomePageProps> = () => {
   return (
-    <div className="container relative min-h-screen max-w-[1440px] text-3xl font-bold mx-auto font-gt flex flex-col">
+    <div className="container relative min-h-screen min-h-[860px] max-w-[1440px] text-3xl font-bold mx-auto font-gt flex flex-col">
       <div className="h-[40px] hidden screen-prime:block bg-transparent" />
       <Header />
       <div className="flex flex-1 bg-prime-white screen-prime:rounded-b-xl">
